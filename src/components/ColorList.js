4862bfdc8456efdb32ad177019a4baf4b858b485
garrettdick <garrettd0511@gmail.com>
2021-10-16 16:04:44 -0500

    commit tree d5ae10cc52b45e5e7c8ff1c7f0fda146278239ed

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -1,4 +1,6 @@
 import React from "react";
+import ColorItem from './ColorItem';
+
 
 function ColorList() {
   const colors = [
@@ -7,17 +9,31 @@ function ColorList() {
     "salmon",
     "darkslategray",
     "hotpink",
+    "blue",
+    "organge"
   ];
+
+  const colorElements = colors.map((color) => {
+    return <ColorItem key={color} color={color} />
+  })
+
+
+
+  // This was practice using Keys and ID's for Objects
+  // const users = [
+  // { id: 1, firstName: "Duane", lastName: "Watson" },
+  // { id: 2, firstName: "Duane", lastName: "Johnson" },
+  // ];
+
+  // const usersHeading = users.map((users) => {
+  //   return <h1 key={users.id}>{users.firstName}</h1>
+  // })
 
   return (
     <div>
       <h1>Top 5 CSS Colors</h1>
       <ol>
-        <li style={{ color: colors[0] }}>{colors[0]}</li>
-        <li style={{ color: colors[1] }}>{colors[1]}</li>
-        <li style={{ color: colors[2] }}>{colors[2]}</li>
-        <li style={{ color: colors[3] }}>{colors[3]}</li>
-        <li style={{ color: colors[4] }}>{colors[4]}</li>
+        {colorElements}
       </ol>
     </div>
   );
